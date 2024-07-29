1231c51e169036105d396899c322020ed9277840
Edson Paco <edsonpaco56@gmail.com>
2024-07-29 17:45:04 +0200

    Actualizacion de portafolio

---
+++ script.js
@@ -0,0 +1,50 @@
+let menuVisible = false;
+//Función que oculta o muestra el menu
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
+
+function seleccionar(){
+    //oculto el menu una vez que selecciono una opcion
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
+//Funcion que aplica las animaciones de las habilidades
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[1].classList.add("htmlcss");
+        habilidades[2].classList.add("javaCpython");
+        habilidades[3].classList.add("wordpress");
+        habilidades[4].classList.add("basedatos");
+        habilidades[5].classList.add("servidores");
+        habilidades[6].classList.add("auditoria");
+        habilidades[7].classList.add("comunicacion");
+        habilidades[8].classList.add("equipo");
+        habilidades[9].classList.add("creatividad");
+        habilidades[10].classList.add("dedicacion");
+        habilidades[11].classList.add("diseño");
+        habilidades[12].classList.add("metodo");
+        habilidades[13].classList.add("modelo");
+        habilidades[0].classList.add("js");
+
+
+
+
+    }
+}
+
+
+//detecto el scrolling para aplicar la animacion de la barra de habilidades
+window.onscroll = function(){
+    efectoHabilidades();
+} 
+
